edc234012360b38c63fa3dc0e927393967476ab5
@Wil <emma.wilinston@gmail.com>
2022-04-08 17:59:50 -0500

    HomeScreen fix scroll view

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,31 +2,41 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import Card from './components/Card';
 import Images from '../../assets/Images';
+import {ScrollView} from 'react-native';
+import colors from '../../constants/colors';
 
 const HomeScreen = () => {
   return (
     <Layout title="Inicio">
-      <Card
-        productImg={Images.product1}
-        brandImg={Images.productBrand}
-        title="Max Factor"
-        time="20 min"
-        subtitle="Prueba el nueva Lash Crown Mascara"
-      />
-      <Card
-        productImg={Images.product2}
-        brandImg={Images.productBrand}
-        title="Max Factor"
-        time="20 min"
-        subtitle="Prueba el nueva Lash Crown Mascara"
-      />
-      <Card
-        productImg={Images.product1}
-        brandImg={Images.productBrand}
-        title="Max Factor"
-        time="20 min"
-        subtitle="Prueba el nueva Lash Crown Mascara"
-      />
+      <ScrollView
+        style={{
+          backgroundColor: colors.mainColor,
+          paddingTop: 10,
+          paddingHorizontal: 15,
+        }}
+      >
+        <Card
+          productImg={Images.product1}
+          brandImg={Images.productBrand}
+          title="Max Factor"
+          time="20 min"
+          subtitle="Prueba el nueva Lash Crown Mascara"
+        />
+        <Card
+          productImg={Images.product2}
+          brandImg={Images.productBrand}
+          title="Max Factor"
+          time="20 min"
+          subtitle="Prueba el nueva Lash Crown Mascara"
+        />
+        <Card
+          productImg={Images.product1}
+          brandImg={Images.productBrand}
+          title="Max Factor"
+          time="20 min"
+          subtitle="Prueba el nueva Lash Crown Mascara"
+        />
+      </ScrollView>
     </Layout>
   );
 };
